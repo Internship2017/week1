f720611cfa6dc3703abc94b2f7465fa5fe0ee0c9
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2017-06-07 16:46:14 -0500

    Solution to 04

--- 04/practice.rb
+++ 04/practice.rb
@@ -12,4 +12,43 @@ require 'github_api'
 
 ## Your code starts here
 
+module Ghub
+
+  class User
+    attr_accessor :name, :repos, :number_of_repos
+
+    def initialize(name)
+      github = Github.new basic_auth: ''
+      @name = name
+      @repos = github.repos.list user: @name
+      @repos = @repos.map { |repo_raw| Repo.new(repo_raw) }
+      @number_of_repos = @repos.length
+    end
+  end
+
+  class Repo
+    attr_accessor :name, :description, :stargazers_count, :url
+
+    def initialize(repo_raw)
+      @name = repo_raw["name"]
+      @description = repo_raw["description"]
+      @stargazers_count = repo_raw["stargazers_count"]
+      @url = repo_raw["clone_url"]
+    end
+  end
+end
+
+
+github_user = Ghub::User.new('lmauromb')
+
+puts "User: #{github_user.name}"
+puts "Total of repos: #{github_user.number_of_repos}"
+
+github_user.repos.each do |repo|
+  puts "Name: #{repo.name}"
+  puts "Description: #{repo.description}"
+  puts "Stars: #{repo.stargazers_count}"
+  puts "URL: #{repo.url}"
+end
+
 ## Your code ends here
