78056a8d080cf6170a095eeca9c1e88d9db4cf8a
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2017-06-08 15:34:44 -0500

    Solve 07

---
+++ 07/output_markdown.md
@@ -0,0 +1,26 @@
+# This Week GitHub Trending Repositories
+
+## sdmg15/Best-websites-a-programmer-should-visit
+- Stars: **7973**
+- Forks: **582**
+
+## colebemis/feather
+- Stars: **4040**
+- Forks: **85**
+
+## karan/Projects
+- Stars: **3804**
+- Forks: **320**
+
+## leandromoreira/digital_video_introduction
+- Stars: **3759**
+- Forks: **164**
+
+## kailashahirwar/cheatsheets-ai
+- Stars: **2483**
+- Forks: **333**
+
+## monicahq/monica
+- Stars: **1772**
+- Forks: **129**
+
--- 07/practice.rb
+++ 07/practice.rb
@@ -7,9 +7,65 @@
 
 require 'rack'
 require 'erb'
-require 'debugger'
-Dir["./support/*.rb"].each {|file| require file }
+# require 'debugger'
+# Dir["./support/*.rb"].each {|file| require file }
 
 # Your code starts here
+require 'rest-client'
+require 'nokogiri'
+require 'github/markup'
 
+class Page
+
+  def render
+    ERB.new(File.read('./layout.erb')).result(binding)
+  end
+
+  def content
+    render do
+      GitHub::Markup.render('output_markdown.md', File.read('./output_markdown.md'))
+    end
+  end
+
+end
+
+# Read the html
+document = RestClient.get('https://github.com/explore')
+# Model the DOM
+dom = Nokogiri::HTML(document)
+# Traverse the DOM
+trending_repos = []
+
+explore_trending = dom.css('#explore-trending')
+
+dom.css('#explore-trending').css('.clearfix li').each do |trending_repo|
+  repo_name = trending_repo.css('a').text
+  repo_forks = trending_repo.css('span')[0].text.sub(',','').to_i
+  repo_stars = trending_repo.css('span')[1].text.sub(',','').to_i
+  trending_repos << {
+    repo_name: repo_name,
+    repo_forks: repo_forks,
+    repo_stars: repo_stars
+  }
+end
+
+# Write a File
+
+file = File.open('./output_markdown.md', 'w+')
+file.write("# This Week GitHub Trending Repositories\n\n")
+trending_repos.each do |repo|
+  file.write("## #{repo[:repo_name]}\n")
+  file.write("- Stars: **#{repo[:repo_stars]}**\n")
+  file.write("- Forks: **#{repo[:repo_forks]}**\n\n")
+end
+file.close
+
+
+webapp = -> (env) {
+  ['200', {'Content-Type' => 'text/html'}, [ Page.new.content ] ]
+}
+
+# Rack::Handler::WEBrick
+# Webrick Server
+Rack::Handler::WEBrick.run webapp, Host: "0.0.0.0"
 # Your code ends here
